04344f649d03b4f1c6d2a4ea36d334b9b67de622
Ahmed Kamal <a.abdelfatah@live.com>
2014-03-15 06:05:28 +0200

    SNWS works !

---
+++ SNWCF/SNDataService.svc.cs
@@ -0,0 +1,36 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Data.Services.Providers;
+using System.Web;
+
+namespace SNWCF
+{
+    [System.ServiceModel.ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+
+    public class SNDataService : EntityFrameworkDataService<SmartNewsEntities>
+    {
+
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
+            config.SetServiceOperationAccessRule("*", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+            config.UseVerboseErrors = true;
+            
+
+            
+        }
+    }
+}
